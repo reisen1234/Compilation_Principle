ebb0dc9b919a05271c76767ffff5583ea95ded98
纳西妲单推人 <2843542905@qq.com>
2025-09-09 17:31:01 +0800

    Add files via upload

---
+++ 5/LR(1)文法.cpp
@@ -0,0 +1,492 @@
+#include <iostream>
+#include <fstream>
+#include<stdio.h>
+#include <string>
+#include <stack>
+#include <map>
+#include <unordered_set>
+#include <set>
+#include<unordered_map>
+#include <vector>
+#include<queue>
+#include<string>
+#include<algorithm>
+using namespace std;
+struct NODE {
+	char begin;
+	string next;
+	NODE(char begin, string next) {
+		this->begin = begin;
+		this->next = next;
+	}
+	NODE() {
+		this->begin = NULL;
+		this->next = "";
+	}
+};
+struct ITEM {
+	string value;
+	NODE node;
+	char next;
+	char next_char;
+	set<char> successor;
+	int Successor_Status;
+	ITEM(string value, NODE node, char next, set<char> successor, int Successor_Status, char next_char) {
+		this->next = next;
+		this->node = node;
+		this->successor = successor;
+		this->value = value;
+		this->Successor_Status = Successor_Status;
+		this->next_char = next_char;
+	}
+	ITEM() {
+		this->next = ' ';
+		this->next_char = ' ';
+		this->Successor_Status = -1;
+		this->node = NODE();
+		this->successor = set<char>{};
+		this->value = "";
+	};
+};
+struct SetOfItems
+{
+	set<string>closures;
+	set<string>deduplication_set;
+	vector<ITEM> items;
+	int Prev_Status;
+	char Prev_Char;
+	int index;
+	SetOfItems(set<string> closures, vector<ITEM> items, set<string>deduplication_set, int Prev_Status, char Prev_Char, int index) {
+		this->closures = closures;
+		this->items = items;
+		this->deduplication_set = deduplication_set;
+		this->Prev_Char = Prev_Char;
+		this->Prev_Status = Prev_Status;
+		this->index = index;
+	}
+	SetOfItems() {
+		this->closures = set<string>{};
+		this->deduplication_set = set<string>{};
+		this->index = -1;
+		this->items = vector<ITEM>{};
+		this->Prev_Char = ' ';
+		this->Prev_Status = -1;
+	};
+};
+string text = "./Grammatical/Grammatical10.txt";
+char Start;//开始符号
+unordered_set<char> VT;//终结符
+unordered_set<char>VN; //非终结符
+vector<char>Vn;//输入顺序排序的终结符
+unordered_map<char, vector<string>>Epsilon;//非终结符对应的产生式
+vector<string>Origin_Grammatical;//保存输入的文法
+unordered_map<int, string> Epsilon_Index;
+unordered_map<string, int> Epsilon_index_map;//产生式对应条数
+unordered_set<string> Deduplication_Set;//去重集合
+vector<SetOfItems>Analyze_Table;//LR(1)项目集规范组
+unordered_map<char, unordered_set<char>> First;//first集
+unordered_map<int, unordered_map<char, string>>ACTION_TABLE;//ACTION表
+unordered_map<int, unordered_map<char, int>>GOTO_TABLE;//GOTO表
+unordered_map<int, unordered_set<int>>Replace_Table;//LALR替换表
+int Epsilon_index = 0;
+//将数字转化为字符串
+string Number_Turn_String(int number) {
+	if (number == 0)return "0";
+	string ans = "";
+	while (number) {
+		ans += (number % 10) + '0';
+		number /= 10;
+	}
+	return string(ans.rbegin(), ans.rend());
+}
+//打印产生式集合
+void Printf_Epsilon_Set() {
+	cout << "生成的产生式是:" << endl;
+	int i = 0;
+	for (int i = 0; i < Epsilon_Index.size(); i++) {
+		cout << "\t" << i << ":" << Epsilon_Index[i] << endl;
+	}
+}
+//符号分类，初始化
+void Sort_Char() {
+	//构建非终结符表
+	for (int i = 0; i < Origin_Grammatical.size(); i++) {
+		string value = Origin_Grammatical[i];
+		char vn = value[0];
+		if (i == 0)Start = vn;
+		VN.insert(vn);
+		Vn.push_back(vn);
+	}
+	VT.insert('$');
+	//构建终结符表,产生式
+	for (int i = 0; i < Origin_Grammatical.size(); i++) {
+		string value = Origin_Grammatical[i];
+		char Begin_VN = value[0];
+		int j = 0;
+		string token = "";
+		char vt;
+		for (j = 1; j < value.size() - 1; j++) {
+			if (value[j - 1] == '-' && value[j] == '>')break;
+		}
+		string temp_value = "";
+		for (int k = j + 1; k < value.size(); k++) {
+			if (value[k] == '|') {
+				Epsilon[Begin_VN].push_back(token);
+				temp_value = string(1, Begin_VN);
+				temp_value += ("->" + token);
+				Epsilon_Index[Epsilon_index] = temp_value;
+				Epsilon_index_map[temp_value] = Epsilon_index++;
+				token = "";
+				continue;
+			}
+			else if (value[k] != ' ') {
+				vt = value[k];
+				if (vt != '#' && VN.find(vt) == VN.end())
+					VT.insert(vt);
+			}
+			if (value[k] != ' ')token += value[k];
+		}
+		if (token != "") {
+			Epsilon[Begin_VN].push_back(token);
+			temp_value = string(1, Begin_VN);
+			temp_value += ("->" + token);
+			Epsilon_Index[Epsilon_index] = temp_value;
+			Epsilon_index_map[temp_value] = Epsilon_index++;
+		}
+	}
+}
+//初始化 
+void init() {
+	fstream In(text);
+	string val = "";
+	while (getline(In, val)) {
+		if (val != "" && val != "\n")
+			Origin_Grammatical.push_back(val);
+		else break;
+	}
+	Sort_Char();
+}
+//集合转化为字符串
+string SetToString(set<char> temp) {
+	string ans = "";
+	for (char c : temp)ans += c;
+	return ans;
+}
+//给文法加点
+string Create_Next_Dot(string s) {
+	int dot_flag = -1;
+	for (int i = 0; i < s.size(); i++)
+		if (s[i] == '.')dot_flag = i;
+	if (dot_flag == -1)
+		return "." + s;
+	else if (dot_flag == s.size() - 1)
+		return "acc";
+	else
+		return s.substr(0, dot_flag) + s[dot_flag + 1] + "." + s.substr(dot_flag + 2, s.size() - dot_flag - 2);
+}
+//去掉文法的点
+string Remove_Dot(string s) {
+	int dot_flag = -1;
+	for (int i = 0; i < s.size(); i++)
+		if (s[i] == '.')dot_flag = i;
+	if (dot_flag == -1)return s;
+	else return s.substr(0, dot_flag) + s.substr(dot_flag + 1, s.size() - dot_flag - 1);
+}
+//获取文法点后的字符
+int Get_Next_Char(string s) {
+	for (int i = 0; i < s.size(); i++) {
+		if (s[i] == '.') {
+			if (i == s.size() - 1)return -1;
+			return i + 1;
+		}
+	}
+	return -1;
+}
+//找到非终结符对应的第一个终结符
+void Find_First_Value(char origin, char c, string P) {
+	stack<char>temp;
+	temp.push(c);
+	while (!temp.empty()) {
+		char T = temp.top();
+		temp.pop();
+		vector<string> tempValue = Epsilon.find(T)->second;
+		for (int i = 0; i < tempValue.size(); i++) {
+			if (VN.find(tempValue[i][0]) != VN.end()) {
+				temp.push(tempValue[i][0]);
+				if (tempValue[i][0] == T)temp.pop();
+			}
+			else First[origin].insert(tempValue[i][0]);
+		}
+	}
+}
+//构造First集
+void Get_First_Set() {
+	for (auto it = VN.begin(); it != VN.end(); it++) {
+		vector<string> value = Epsilon.find(*it)->second;
+		for (int i = 0; i < value.size(); i++) {
+			if (VN.find(value[i][0]) != VN.end()) {
+				if (*it != value[i][0])Find_First_Value(*it, value[i][0], value[i]);
+			}
+			else First[*it].insert(value[i][0]);
+		}
+	}
+}
+//获取后继符号
+set<char> Get_First_Set(char b, set<char> a) {
+	set<char> c;
+	if (b == '$')return a;
+	if (VN.find(b) != VN.end()) {
+		a.clear();
+		for (char c : First.find(b)->second)if (c != '$')a.insert(c);
+		return a;
+	}
+	else {
+		c.insert(b);
+		return c;
+	}
+}
+//初始化集合
+SetOfItems initClosure() {
+	string epsilon = Epsilon.find(Start)->second[0];
+	string Next_Dot_String = Create_Next_Dot(epsilon);
+	int next_dot_index = Get_Next_Char(Next_Dot_String);
+	char next_dot_char = next_dot_index == -1 ? '$' : Next_Dot_String[next_dot_index];
+	set<char>head{ '$' };
+	string value = string(1, Start) + "->" + Next_Dot_String;
+	set<string>closures{ value };
+	set<string>New_Deduplication_Set{ value + SetToString(head) };
+	vector<ITEM> items{ ITEM(value, NODE(Start, Next_Dot_String), next_dot_char, head, -1, '$') };
+	return SetOfItems(closures, items, New_Deduplication_Set, 0, ' ', 0);
+}
+bool Is_Same(set<string> set1, set<string> set2) {
+	if (set1.size() != set2.size())return false;
+	set<string>set3;
+	set_intersection(set1.begin(), set1.end(), set2.begin(), set2.end(), inserter(set3, set3.begin()));
+	if (set3.size() == set1.size() && set3.size() == set2.size())return true;
+	return false;
+}
+int Is_Set_Same(set<string> set1) {
+	for (int i = 0; i < Analyze_Table.size(); i++)
+		if (Is_Same(Analyze_Table[i].deduplication_set, set1))return i;
+	return -1;
+}
+//设置后继符
+void setSuccessor(unordered_set<char> b, ITEM& Origin) {
+	for (char c : b)Origin.successor.insert(c);
+}
+//求状态I闭包
+SetOfItems* CLOSURE(SetOfItems I) {
+	vector<ITEM> NEW_STATE;
+	unordered_set<char>Deduplication_Char;
+	set<string> NEW_ITEMS;
+	set<string>New_Deduplication_Set;
+	set<string>Deduplication_Set_;
+	for (ITEM item : I.items) {
+		NEW_STATE.push_back(item);
+		NEW_ITEMS.insert(item.value);
+		Deduplication_Set_.insert(item.value + SetToString(item.successor));
+	}
+	for (ITEM item : I.items) {
+		stack<ITEM>ITEM_STACK;
+		ITEM_STACK.push(item);
+		while (!ITEM_STACK.empty()) {
+			ITEM S = ITEM_STACK.top();
+			ITEM_STACK.pop();
+			if (VT.find(S.next) != VT.end())continue;
+			vector<string>epsilon = Epsilon.find(S.next)->second;
+			for (string s : epsilon) {
+				string Next_Dot_String = Create_Next_Dot(s);
+				int next_dot_index = Get_Next_Char(Next_Dot_String);
+				char next_dot_char = next_dot_index == -1 ? '$' : Next_Dot_String[next_dot_index];
+				char next_char;
+				if (next_dot_index == -1) {
+					next_dot_char = '$';
+					next_char = '$';
+				}
+				else {
+					if (Next_Dot_String.size() - 1 == next_dot_index)next_char = '$';
+					else next_char = Next_Dot_String[next_dot_index + 1];
+				}
+				string value = string(1, S.next) + "->" + Next_Dot_String;
+				set<char> New_successor = Get_First_Set(S.next_char, S.successor);
+				if (NEW_ITEMS.find(value) != NEW_ITEMS.end()) {
+					for (int i = 0; i < NEW_STATE.size(); i++)
+						if (NEW_STATE[i].value == value) {
+							if (Deduplication_Set_.find(value + SetToString(New_successor)) != Deduplication_Set_.end())break;
+							for (char c : New_successor)NEW_STATE[i].successor.insert(c);
+							Deduplication_Set_.insert(value + SetToString(New_successor));
+							ITEM_STACK.push(ITEM(value, NODE(S.next, Next_Dot_String), next_dot_char, New_successor, -1, next_char));
+							break;
+						}
+					continue;
+				}
+				string D = value + SetToString(New_successor);
+				if (Deduplication_Set_.find(D) != Deduplication_Set_.end())continue;
+				ITEM NEW_ITEM = ITEM(value, NODE(S.next, Next_Dot_String), next_dot_char, New_successor, -1, next_char);
+				NEW_STATE.push_back(NEW_ITEM);
+				NEW_ITEMS.insert(value);
+				ITEM_STACK.push(NEW_ITEM);
+				Deduplication_Set_.insert(D);
+			}
+		}
+	}
+	for (ITEM item : NEW_STATE) {
+		string value = item.value + SetToString(item.successor);
+		Deduplication_Set.insert(value);
+		New_Deduplication_Set.insert(value);
+	}
+	SetOfItems* Ans = new SetOfItems(NEW_ITEMS, NEW_STATE, New_Deduplication_Set, I.Prev_Status, I.Prev_Char, I.index);
+	return Ans;
+}
+//由状态I通过X条件转换为另一个状态
+SetOfItems* GOTO(SetOfItems I, int i, char X) {
+	set<string>closures;
+	set<string>deduplication_set;
+	vector<ITEM> items;
+	for (ITEM item : I.items) {
+		if (item.next != X)continue;
+		string Next_Dot_String = Create_Next_Dot(item.node.next);
+		int next_dot_index = Get_Next_Char(Next_Dot_String);
+		char next_dot_char = next_dot_index == -1 ? '$' : Next_Dot_String[next_dot_index];
+		char next_char;
+		if (next_dot_index == -1) {
+			next_dot_char = '$';
+			next_char = '$';
+		}
+		else {
+			if (Next_Dot_String.size() - 1 == next_dot_index)next_char = '$';
+			else next_char = Next_Dot_String[next_dot_index + 1];
+		}
+		string value = string(1, item.node.begin) + "->" + Next_Dot_String;
+		ITEM NEW_ITEM = ITEM(value, NODE(item.node.begin, Next_Dot_String), next_dot_char, item.successor, -1, next_char);
+		closures.insert(value);
+		deduplication_set.insert(value + SetToString(item.successor));
+		items.push_back(NEW_ITEM);
+	}
+	SetOfItems* New_Item = CLOSURE(SetOfItems(closures, items, deduplication_set, i, X, (int)Analyze_Table.size()));
+	int flag = Is_Set_Same(New_Item->deduplication_set);
+	if (flag != -1) {
+		for (ITEM& item : Analyze_Table[i].items)
+			if (item.next == X)item.Successor_Status = flag;
+		return NULL;
+	}
+	return New_Item;
+}
+//填写转换状态
+void Fill_Out_Successor_Status(int i, int target, char s) {
+	for (ITEM& item : Analyze_Table[i].items) {
+		if (item.Successor_Status != -1)continue;
+		if (item.next == s)item.Successor_Status = target;
+	}
+}
+//求GOTO图
+void items() {
+	//构造GOTO图
+	SetOfItems Begin = *CLOSURE(initClosure());
+	Analyze_Table.push_back(Begin);
+	queue<SetOfItems>LR_Queue;
+	LR_Queue.push(Begin);
+	while (!LR_Queue.empty()) {
+		SetOfItems S = LR_Queue.front();
+		LR_Queue.pop();
+		for (ITEM item : S.items) {
+			if (item.next == '$')continue;
+			SetOfItems* New_LR_Item = GOTO(S, S.index, item.next);
+			if (New_LR_Item == NULL)continue;
+			Analyze_Table.push_back(*New_LR_Item);
+			LR_Queue.push(*New_LR_Item);
+		}
+	}
+	//填入GOTO图后继状态
+	for (int i = 1; i < Analyze_Table.size(); i++) {
+		Fill_Out_Successor_Status(Analyze_Table[i].Prev_Status, i, Analyze_Table[i].Prev_Char);
+	}
+	for (int i = 0; i < Analyze_Table.size(); i++)
+		for (ITEM& item : Analyze_Table[i].items) {
+			if (item.Successor_Status == -1) {
+				item.Successor_Status = (int)Analyze_Table.size();
+				if (item.node.begin == Start) {
+					ACTION_TABLE[i]['$'] = "acc";
+					continue;
+				}
+				for (char c : item.successor) {
+					ACTION_TABLE[i][c] = "r" + Number_Turn_String(Epsilon_index_map[Remove_Dot(item.value)]);
+				}
+			}
+			else {
+				if (VN.find(item.next) != VN.end()) GOTO_TABLE[i][item.next] = item.Successor_Status;
+				else ACTION_TABLE[i][item.next] = "S" + Number_Turn_String(item.Successor_Status);
+			}
+
+		}
+}
+//打印SLR(1)各种表格
+void Printf_(int number) {
+	for (int i = 0; i < number; i++)cout << "------";
+}
+string SetToString_(set<char> s) {
+	string ans = "";
+	for (char c : s) {
+		ans += c;
+		ans += '/';
+	}
+	ans.pop_back();
+	return ans;
+}
+void Printf_LR1_GOTO() {
+	cout << "LR(1)分析表:\n\n";
+	printf("%-8s%-16s%-16s%-8s", "状态", "项目集", "后继符", "后继状态\n");
+	Printf_(8);
+	for (int i = 0; i < Analyze_Table.size(); i++) {
+		printf("\n%s%-7d", "S", i);
+		for (int j = 0; j < Analyze_Table[i].items.size(); j++) {
+			string value = Analyze_Table[i].items[j].value;
+			string Next_Char = SetToString_(Analyze_Table[i].items[j].successor);
+			int Successor_Status = Analyze_Table[i].items[j].Successor_Status;
+			if (j == 0)printf("%-16s", value.c_str());
+			else printf("%-8s%-16s", "", value.c_str());
+			printf("%-16s%s%-8d\n", Next_Char.c_str(), "S", Successor_Status);
+		}
+		Printf_(8);
+		cout << endl;
+	}
+}
+void Printf_ACTION_GOTO() {
+	cout << "\nACTION表:\n";
+	printf("%-6s\t%s\n", "状", "ACTION"); printf("%-6s", "态");
+	for (char c : VT) {
+		printf("%-6c", c);
+	}
+	cout << "|" << endl; Printf_((int)VT.size() + 1); cout << endl;
+	for (int i = 0; i < Analyze_Table.size(); i++) {
+		string In = "S" + Number_Turn_String(i);
+		printf("%-6s", In.c_str());
+		for (char c : VT) {
+			printf("%-6s", ACTION_TABLE[i][c].c_str());
+		}
+		cout << "|" << endl; Printf_((int)VT.size() + 1); cout << endl;
+	}
+	cout << "\nGOTO表:\n";
+	printf("%-6s\t%s\n", "状", "GOTO"); printf("%-6s", "态");
+	for (char c : VN) if (c != Start)printf("%-6c", c);
+	cout << "|" << endl; Printf_((int)VN.size()); cout << endl;
+	for (int i = 0; i < Analyze_Table.size(); i++) {
+		string In = "S" + Number_Turn_String(i);
+		printf("%-6s", In.c_str());
+		for (char c : VN) {
+			if (c == Start)continue;
+			if (GOTO_TABLE[i][c] == 0)printf("%-6s", "");
+			else printf("%-6d", GOTO_TABLE[i][c]);
+		}
+		cout << "|" << endl; Printf_((int)VN.size()); cout << endl;
+	}
+}
+int main() {
+	init();
+	Printf_Epsilon_Set();
+	Get_First_Set();
+	items();
+	Printf_LR1_GOTO();
+	Printf_ACTION_GOTO();
+	return 0;
+}
